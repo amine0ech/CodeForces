11b68b6626fa094d0f332bf82051d2ff17b60277
amine0ech <amineechchaa@gmail.com>
2025-04-21 14:17:34 +0100

    add problems

--- A/Buy_a_Shovel.cpp
+++ A/Buy_a_Shovel.cpp
@@ -30,10 +30,22 @@ const ld EPS = 1e-9;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    
-    
-    
+
+    int price , deno ; 
+    cin >> price >> deno ; 
+    if(price % 10 == 0) {
+        cout << 1 ; return 0 ; 
+    }
+
+    for(int i =1 ; i <= 9 ; i++){
+        if(price * i  % 10 == deno || price * i % 10 == 0){
+            cout << i ; 
+            return 0 ; 
+        }
+    }
+
+    cout << 10 ; 
 
 
-    
+    return 0;    
 }
